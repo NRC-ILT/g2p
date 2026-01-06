39cf0d864fee3a6d9c135eede27bde46190cf55e
Eric Joanis <eric.joanis@nrc-cnrc.gc.ca>
2026-01-06 14:17:15 -0500

    test: skip playwright tests if browsers are not installed/launchable

--- g2p/tests/test_studio.py
+++ g2p/tests/test_studio.py
@@ -14,9 +14,10 @@ or robust server mode (*nix only, gunicorn does not work on Windows):
 
 from datetime import datetime
 from random import sample
-from unittest import IsolatedAsyncioTestCase, main
+from unittest import IsolatedAsyncioTestCase, SkipTest, main
 
 import socketio  # type: ignore
+from playwright.async_api import Error as PlaywrightError  # type: ignore
 from playwright.async_api import async_playwright, expect  # type: ignore
 
 from g2p.log import LOGGER
@@ -39,7 +40,10 @@ class StudioTest(IsolatedAsyncioTestCase):
 
     async def test_sanity(self):
         async with async_playwright() as p:
-            browser = await p.chromium.launch(channel="chrome", headless=True)
+            try:
+                browser = await p.chromium.launch(channel="chrome", headless=True)
+            except PlaywrightError:
+                raise SkipTest("Cannot launch browser, skipping playwright tests")
             page = await browser.new_page()
             await page.goto(f"http://127.0.0.1:{self.port}/docs")
             await page.wait_for_timeout(self.timeout_delay)
@@ -65,7 +69,10 @@ class StudioTest(IsolatedAsyncioTestCase):
 
     async def test_switch_langs(self):
         async with async_playwright() as p:
-            browser = await p.chromium.launch(channel="chrome", headless=True)
+            try:
+                browser = await p.chromium.launch(channel="chrome", headless=True)
+            except PlaywrightError:
+                raise SkipTest("Cannot launch browser, skipping playwright tests")
             page = await browser.new_page()
             await page.goto(f"http://127.0.0.1:{self.port}")
             await page.wait_for_timeout(self.timeout_delay)
@@ -155,7 +162,10 @@ class StudioTest(IsolatedAsyncioTestCase):
             LOGGER.info("Lauching async_playwright")
             async with async_playwright() as p:
                 LOGGER.info(("L" if block == 0 else "Rel") + "aunching browser")
-                browser = await p.chromium.launch(channel="chrome", headless=True)
+                try:
+                    browser = await p.chromium.launch(channel="chrome", headless=True)
+                except PlaywrightError:
+                    raise SkipTest("Cannot launch browser, skipping playwright tests")
                 LOGGER.info("Loading page")
                 page = await browser.new_page()
                 await page.goto(
