e544c777a0e0db2671949de288f9f0071891261a
Eric Joanis <eric.joanis@nrc-cnrc.gc.ca>
2026-04-16 18:36:35 -0400

    ci: move to using pytest by default and in ci

--- g2p/tests/test_neural.py
+++ g2p/tests/test_neural.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import sys
-from unittest import SkipTest, TestCase, main
+from unittest import SkipTest, TestCase
+
+import pytest
 
 from g2p import make_g2p
 from g2p.log import LOGGER
@@ -56,4 +58,4 @@ class NeuralLangTest(TestCase):
 
 
 if __name__ == "__main__":
-    main()
+    pytest.main(["-v", __file__])
